913ec16c9e90fedb632e85c02fe78575fe3336c4
sandeshjadhav5 <jadhavs932@gmail.com>
2023-05-13 11:12:59 +0530

    added tools separately

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -253,28 +253,7 @@ const Skills = () => {
             Node JS
           </Text>
         </Box>
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          colSpan={1}
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
-            m="auto"
-            w="80px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            Git
-          </Text>
-        </Box>
+
         <Box
           data-aos="zoom-in"
           data-aos-duration="700"
@@ -353,6 +332,28 @@ const Skills = () => {
           className="skills-card-img"
         >
           <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/600px-Tailwind_CSS_Logo.svg.png?20211001194333"
+            m="auto"
+            w="60px"
+            h="50px"
+          />
+          <Text className="skills-card-name" mt={5} fontWeight={600}>
+            Tailwind
+          </Text>
+        </Box>
+
+        <Box
+          data-aos="zoom-in"
+          data-aos-duration="700"
+          w="100%"
+          m="auto"
+          rounded="md"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          className="skills-card-img"
+        >
+          <Image
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png?20210507000024"
             m="auto"
             w="60px"
@@ -362,6 +363,24 @@ const Skills = () => {
             BootStrap
           </Text>
         </Box>
+      </SimpleGrid>
+      <Heading
+        textAlign="center"
+        mb="25"
+        color="#48BB78"
+        data-aos="zoom-in"
+        data-aos-duration="700"
+      >
+        Tools
+      </Heading>
+      <SimpleGrid
+        minChildWidth={{ lg: "120px", md: "100px", sm: "100px", base: "100px" }}
+        w="80%"
+        m="auto"
+        gap={6}
+        mb="50"
+        className="skills-card"
+      >
         <Box
           data-aos="zoom-in"
           data-aos-duration="700"
@@ -381,6 +400,156 @@ const Skills = () => {
           />
           <Text className="skills-card-name" mt={5} fontWeight={600}>
             npm
+          </Text>
+        </Box>
+        <Box
+          data-aos="zoom-in"
+          data-aos-duration="700"
+          colSpan={1}
+          w="100%"
+          m="auto"
+          rounded="md"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          className="skills-card-img"
+        >
+          <Image
+            src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
+            m="auto"
+            w="80px"
+            h="50px"
+          />
+          <Text className="skills-card-name" mt={5} fontWeight={600}>
+            Git
+          </Text>
+        </Box>
+        <Box
+          data-aos="zoom-in"
+          data-aos-duration="700"
+          colSpan={1}
+          w="100%"
+          m="auto"
+          rounded="md"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          className="skills-card-img"
+        >
+          <Image
+            src="https://res.cloudinary.com/postman/image/upload/t_team_logo/v1629869194/team/2893aede23f01bfcbd2319326bc96a6ed0524eba759745ed6d73405a3a8b67a8"
+            m="auto"
+            w="80px"
+            h="50px"
+          />
+
+          <Text className="skills-card-name" mt={5} fontWeight={600}>
+            Postman
+          </Text>
+        </Box>
+        <Box
+          data-aos="zoom-in"
+          data-aos-duration="700"
+          colSpan={1}
+          w="100%"
+          m="auto"
+          rounded="md"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          className="skills-card-img"
+        >
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519"
+            m="auto"
+            w="80px"
+            h="50px"
+          />
+          <Text className="skills-card-name" mt={5} fontWeight={600}>
+            VS Code
+          </Text>
+        </Box>
+        <Box
+          data-aos="zoom-in"
+          data-aos-duration="700"
+          colSpan={1}
+          w="100%"
+          m="auto"
+          rounded="md"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          className="skills-card-img"
+        >
+          <Image
+            src="https://camo.githubusercontent.com/add2c9721e333f0043ac938f3dadbc26a282776e01b95b308fcaba5afaf74ae3/68747470733a2f2f6173736574732e76657263656c2e636f6d2f696d6167652f75706c6f61642f76313538383830353835382f7265706f7369746f726965732f76657263656c2f6c6f676f2e706e67"
+            m="auto"
+            w="80px"
+            h="50px"
+          />
+          <Text className="skills-card-name" mt={5} fontWeight={600}>
+            Vercel
+          </Text>
+        </Box>
+        <Box
+          data-aos="zoom-in"
+          data-aos-duration="700"
+          colSpan={1}
+          w="100%"
+          m="auto"
+          rounded="md"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          className="skills-card-img"
+        >
+          <Image
+            src="https://jeancochrane.com/static/images/blog/netlify-identity-dealbreakers/netlify-logo.png"
+            m="auto"
+            w="60px"
+            h="50px"
+          />
+
+          <Text className="skills-card-name" mt={5} fontWeight={600}>
+            Netlify
+          </Text>
+        </Box>
+
+        {/* <Box
+          data-aos="zoom-in"
+          data-aos-duration="700"
+          w="100%"
+          m="auto"
+          rounded="md"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          className="skills-card-img"
+        >
+          <Image
+            src="https://rangav.gallerycdn.vsassets.io/extensions/rangav/vscode-thunder-client/2.6.2/1681222207111/Microsoft.VisualStudio.Services.Icons.Default"
+            m="auto"
+            w="60px"
+            h="50px"
+          />
+          <Text className="skills-card-name" mt={5} fontWeight={600}>
+            Thunder Client
+          </Text>
+        </Box> */}
+        <Box
+          data-aos="zoom-in"
+          data-aos-duration="700"
+          w="100%"
+          m="auto"
+          rounded="md"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          className="skills-card-img"
+        >
+          <Image src="https://vitejs.dev/logo.svg" m="auto" w="60px" h="50px" />
+          <Text className="skills-card-name" mt={5} fontWeight={600}>
+            Vite
           </Text>
         </Box>
       </SimpleGrid>
