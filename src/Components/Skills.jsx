b03ee9f6f5fe92464e223a1964a6ec43c93df5ff
sandeshjadhav5 <jadhavs932@gmail.com>
2023-06-26 09:27:58 +0530

    code optimization in skills page

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -17,9 +17,89 @@ import {
 
 const Skills = () => {
   const isDark = useSelector((state) => state.AppReducer.isDark);
+
+  const skillsData = [
+    {
+      id: 1,
+      language: "HTML",
+      imageUrl: "https://cdn.cdnlogo.com/logos/h/84/html.svg",
+    },
+    {
+      id: 2,
+      language: "CSS",
+      imageUrl: "https://static.cdnlogo.com/logos/c/18/css.svg",
+    },
+    {
+      id: 3,
+      language: "JavaScript",
+      imageUrl: "https://static.cdnlogo.com/logos/j/44/javascript.svg",
+    },
+    {
+      id: 4,
+      language: "React",
+      imageUrl: "https://static.cdnlogo.com/logos/r/63/react.svg",
+    },
+    {
+      id: 5,
+      language: "NextJs",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/archive/8/8e/20230404233502%21Nextjs-logo.svg",
+    },
+    {
+      id: 6,
+      language: "Redux",
+      imageUrl: "https://static.cdnlogo.com/logos/r/37/redux.svg",
+    },
+    {
+      id: 7,
+      language: "MongoDB",
+      imageUrl: "https://static.cdnlogo.com/logos/m/25/mongodb.svg",
+    },
+    {
+      id: 8,
+      language: "Express",
+      imageUrl:
+        "https://raw.githubusercontent.com/tomchen/stack-icons/634d5c036a2a7ca0115c94ab2ce86c7e79e01e13/logos/express.svg",
+    },
+    {
+      id: 9,
+      language: "Node JS",
+      imageUrl: "https://static.cdnlogo.com/logos/n/79/node-js.svg",
+    },
+    {
+      id: 10,
+      language: "TypeScript",
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/5968/5968381.png",
+    },
+    {
+      id: 11,
+      language: "Cypress",
+      imageUrl:
+        "https://i0.wp.com/blog.knoldus.com/wp-content/uploads/2022/04/cypress.png?fit=364%2C364&ssl=1",
+    },
+    {
+      id: 12,
+      language: "Tailwind CSS",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/600px-Tailwind_CSS_Logo.svg.png?20211001194333",
+    },
+    {
+      id: 13,
+      language: "Chakra UI",
+      imageUrl: "https://avatars.githubusercontent.com/u/54212428?s=280&v=4",
+    },
+    {
+      id: 14,
+      language: "BootStrap",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png?20210507000024",
+    },
+  ];
+
   useEffect(() => {
     AOS.init();
   }, []);
+
   return (
     <div
       id="skills"
@@ -54,315 +134,27 @@ const Skills = () => {
         mb="50"
         className="skills-card"
       >
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          colSpan={1}
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://cdn.cdnlogo.com/logos/h/84/html.svg"
-            m="auto"
-            w="100px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            HTML
-          </Text>
-        </Box>
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          colSpan={1}
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://static.cdnlogo.com/logos/c/18/css.svg"
-            m="auto"
-            w="72px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            CSS
-          </Text>
-        </Box>
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          colSpan={1}
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://static.cdnlogo.com/logos/j/44/javascript.svg"
-            m="auto"
-            w="72px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            JavaScript
-          </Text>
-        </Box>
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          colSpan={1}
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://static.cdnlogo.com/logos/r/63/react.svg"
-            m="auto"
-            w="82px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            React
-          </Text>
-        </Box>
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          colSpan={1}
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/archive/8/8e/20230404233502%21Nextjs-logo.svg"
-            m="auto"
-            w="82px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            NextJs
-          </Text>
-        </Box>
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          colSpan={1}
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://static.cdnlogo.com/logos/r/37/redux.svg"
-            m="auto"
-            w="82px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            Redux
-          </Text>
-        </Box>
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          colSpan={1}
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://static.cdnlogo.com/logos/m/25/mongodb.svg"
-            m="auto"
-            w="80px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            MongoDB
-          </Text>
-        </Box>
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          colSpan={1}
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://raw.githubusercontent.com/tomchen/stack-icons/634d5c036a2a7ca0115c94ab2ce86c7e79e01e13/logos/express.svg"
-            m="auto"
-            w="80px"
-            h="50px"
-          />
-
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            Express
-          </Text>
-        </Box>
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          colSpan={1}
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://static.cdnlogo.com/logos/n/79/node-js.svg"
-            m="auto"
-            w="80px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            Node JS
-          </Text>
-        </Box>
-
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          colSpan={1}
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://cdn-icons-png.flaticon.com/512/5968/5968381.png"
-            m="auto"
-            w="60px"
-            h="50px"
-          />
-
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            TypeScript
-          </Text>
-        </Box>
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          colSpan={1}
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://i0.wp.com/blog.knoldus.com/wp-content/uploads/2022/04/cypress.png?fit=364%2C364&ssl=1"
-            m="auto"
-            w="70px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            Cypress
-          </Text>
-        </Box>
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://avatars.githubusercontent.com/u/54212428?s=280&v=4"
-            m="auto"
-            w="60px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            Chakra UI
-          </Text>
-        </Box>
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/600px-Tailwind_CSS_Logo.svg.png?20211001194333"
-            m="auto"
-            w="60px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            Tailwind
-          </Text>
-        </Box>
-
-        <Box
-          data-aos="zoom-in"
-          data-aos-duration="700"
-          w="100%"
-          m="auto"
-          rounded="md"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          className="skills-card-img"
-        >
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png?20210507000024"
-            m="auto"
-            w="60px"
-            h="50px"
-          />
-          <Text className="skills-card-name" mt={5} fontWeight={600}>
-            BootStrap
-          </Text>
-        </Box>
+        {skillsData.map((el) => {
+          return (
+            <Box
+              data-aos="zoom-in"
+              data-aos-duration="700"
+              colSpan={1}
+              w="100%"
+              m="auto"
+              rounded="md"
+              alignItems="center"
+              justifyContent="center"
+              textAlign="center"
+              className="skills-card-img"
+            >
+              <Image src={el.imageUrl} m="auto" w="100px" h="50px" />
+              <Text className="skills-card-name" mt={5} fontWeight={600}>
+                {el.language}
+              </Text>
+            </Box>
+          );
+        })}
       </SimpleGrid>
       <Heading
         textAlign="center"
